2a6107eaf9b2931946754d190442a40969d41918
lockhands <30androz@gmail.com>
2022-10-08 12:09:31 -0500

    agregamos todo

---
+++ src/componentes/Testimonio.js
@@ -0,0 +1,18 @@
+import React from'react';
+
+function Testimonio(){
+    return(
+        <div className='contenedor-testimonio'>
+                <img 
+                    className='imagen-testimonio'
+                    src={require('../images/descarga.jpg')}
+                    alt='foto de emma'
+                   />
+            
+                
+        </div>
+
+    );
+}
+
+export default Testimonio;
