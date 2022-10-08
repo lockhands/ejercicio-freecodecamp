2c0d0fbe9879d5dbdc11139ccc679cb70e30f21a
lockhands <30androz@gmail.com>
2022-10-08 18:44:02 -0500

    finalizado

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,18 +1,22 @@
 import React from'react';
+import '../stylesheets/Testimonio.css';
+ 
 
-function Testimonio(){
+export function Testimonio(props){
     return(
         <div className='contenedor-testimonio'>
-                <img 
-                    className='imagen-testimonio'
-                    src={require('../images/descarga.jpg')}
-                    alt='foto de emma'
-                   />
-            
                 
+                <img className='imagen-testimonio'
+                src={require(`../imagenes/Testimonio-${props.imagen}.png`)}
+                alt='Foto de Emma'
+                />
+
+         <div className='contenedor-texto-testimonio'>
+            <p className='nombre-testimonio'>{props.nombre} en {props.pais}</p>    
+            <p className='cargo-testimonio'>{props.cargo} en {props.empresa}</p>
+            <p className='texto-testimonio'>"{props.testimonio}"</p>     </div> 
+
         </div>
 
     );
 }
-
-export default Testimonio;
